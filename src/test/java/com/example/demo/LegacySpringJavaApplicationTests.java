08fe88280b1b101b14a91b6a69b67f900716f629
Edik Walta <Eduard.Walter@gmx.net>
2026-01-15 14:02:11 +0100

    Revert "added failing test"
    This reverts commit 08bc0d4ea3e2eb00fa4f721bbb7ab282035eb62f.

--- src/test/java/com/example/demo/LegacySpringJavaApplicationTests.java
+++ src/test/java/com/example/demo/LegacySpringJavaApplicationTests.java
@@ -2,18 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class LegacySpringJavaApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void failing() {
-		assertEquals(2, 3);
 	}
 
 }
