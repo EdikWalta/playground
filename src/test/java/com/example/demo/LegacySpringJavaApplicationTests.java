08bc0d4ea3e2eb00fa4f721bbb7ab282035eb62f
EdikWalta <Eduard.Walter@gmx.net>
2026-01-15 13:51:39 +0100

    added failing test

--- src/test/java/com/example/demo/LegacySpringJavaApplicationTests.java
+++ src/test/java/com/example/demo/LegacySpringJavaApplicationTests.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class LegacySpringJavaApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void failing() {
+		assertEquals(2, 3);
 	}
 
 }
